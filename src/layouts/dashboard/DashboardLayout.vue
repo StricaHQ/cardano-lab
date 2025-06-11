dac21b44c243fd1af4e62f1ce3625001c2b55768
vorakaran <vorakaran.18@gmail.com>
2025-06-11 15:35:24 +0530

    add footer

--- src/layouts/dashboard/DashboardLayout.vue
+++ src/layouts/dashboard/DashboardLayout.vue
@@ -1,16 +1,20 @@
 <template>
-  <app-header />
-  <app-sub-header />
-  <div class="flex justify-center">
-    <div class="w-full max-w-[1024px] px-5">
-      <div class="mt-16 mb-12">
-        <router-view />
+  <div class="min-h-screen flex flex-col">
+    <app-header />
+    <app-sub-header />
+    <div class="flex justify-center grow h-full">
+      <div class="w-full max-w-[1024px] px-5">
+        <div class="mt-16 mb-12">
+          <router-view />
+        </div>
       </div>
     </div>
+    <app-footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import AppHeader from "./components/AppHeader.vue";
 import AppSubHeader from "./components/AppSubHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
 </script>
---
+++ src/layouts/dashboard/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-primary py-4">
+    <span class="text-sm text-white text-center w-full flex justify-center"
+      >A dev tool by Strica</span
+    >
+  </div>
+</template>
